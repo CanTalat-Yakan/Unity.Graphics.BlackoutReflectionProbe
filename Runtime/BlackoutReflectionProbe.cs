d9b0cd6306cd9c522c97588f6aebcd99e96794f8
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2026-03-07 04:37:55 +0100

    ⠀⠀⠀⠀⠀

--- Runtime/BlackoutReflectionProbe.cs
+++ Runtime/BlackoutReflectionProbe.cs
@@ -7,9 +7,9 @@ namespace UnityEssentials
     public class BlackoutReflectionProbe : MonoBehaviour
     {
         [Info(MessageType.Info)]
-        public const string Info =
-            "This component creates a hidden reflection probe that captures a black cubemap.\n" +
-            "It is useful for preventing unwanted reflections in scenes where no reflections are desired.\n\n" +
+        public string Info =
+            "This component creates a hidden reflection probe that captures a black cubemap." +
+            "It is useful for preventing unwanted reflections in scenes where no reflections are desired." +
             "The scale of the transform defines the dimensions of the reflection probe's bounding box.";
 
         [SerializeField] private bool _hideReflectionProbe = true;
